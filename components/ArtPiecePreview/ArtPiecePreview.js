d4bf24917f906e29cd9c5f7a07a499d67689b5ac
Tolpius <juliusvonbonin@gmail.com>
2025-07-28 17:00:33 +0200

    Fixed some conflics in codebase while merging Next step: Pagination and slugs (Details page)

--- components/ArtPiecePreview/ArtPiecePreview.js
+++ components/ArtPiecePreview/ArtPiecePreview.js
@@ -5,16 +5,22 @@ import toggleFavourite from "../../scripts/toggleFavourites";
 import Link from "next/link";
 
 export default function ArtPiecePreview({ piece }) {
+  const [favourites, setFavourites] = useLocalStorage("favourites", []);
+
+  if (!piece) {
+    return <div>No art piece available</div>;
+  }
+
   return (
-    <Link href={`artPieces/${slug}`} passHref legacyBehavior>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <div style={{ position: "relative", width: "500px" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ position: "relative", width: "500px" }}>
+        <Link href={`artPieces/${piece.slug}`} passHref legacyBehavior>
           <Image
             src={piece.imageSource}
             alt={piece.name}
@@ -22,22 +28,27 @@ export default function ArtPiecePreview({ piece }) {
             height={0}
             style={{ height: "auto", display: "block" }}
           />
-          <HeartIcon
-            size={32}
-            weight={favourites.includes(piece.slug) ? "fill" : "regular"}
-            color="#ff0000"
-            onClick={() => toggleFavourite(piece, favourites, setFavourites)}
-            style={{
-              position: "absolute",
-              top: "8px",
-              right: "8px",
-              background: "white",
-              borderRadius: "50%",
-              padding: "4px",
-            }}
-          />
-        </div>
+        </Link>
+        <HeartIcon
+          size={32}
+          weight={favourites.includes(piece.slug) ? "fill" : "regular"}
+          color="#ff0000"
+          onClick={(e) => {
+            e.preventDefault(); // verhindert Link-Klick beim Icon
+            toggleFavourite(piece, favourites, setFavourites);
+          }}
+          style={{
+            position: "absolute",
+            top: "8px",
+            right: "8px",
+            background: "white",
+            borderRadius: "50%",
+            padding: "4px",
+            cursor: "pointer",
+            zIndex: 2,
+          }}
+        />
       </div>
-    </Link>
+    </div>
   );
 }
