95b82a352e25e80f71084605e20667b8c373f547
Tolpius <juliusvonbonin@gmail.com>
2025-07-28 10:31:02 +0200

    First real commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Footer from '../components/Footer/Footer';
 export default function HomePage() {
   return (
+    <>
     <div>
       <h1>Hello from Next.js</h1>
     </div>
+    <Footer />
+    </>
   );
 }
