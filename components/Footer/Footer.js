5d66d158a1c24c0c5a0cfa15533963366d56e7c3
Milia Laufenberg <coding.l@ufenberg.net>
2025-07-28 10:47:02 +0200

    initiated pages, layout

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,15 +1,27 @@
-import Link from 'next/link';
-import './Footer.css';
+import Link from "next/link";
+import styled from "styled-components";
+
 export default function Footer() {
   return (
-    <footer>
+    <StyledFooter>
       <nav>
         <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
-    </footer>
+    </StyledFooter>
   );
 }
+
+const StyledFooter = styled.footer`
+  display: flex;
+  flex-direction: row;
+`;
